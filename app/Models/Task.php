bd3b8ede67d83753e2cf8af801e05c8ce5dd07cc
MewinDev <merin.johncarlo.prof@gmail.com>
2025-03-28 11:08:43 +0800

    done creating table value using seeder and factory

---
+++ app/Models/Task.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
+class Task extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = [
+        'title',
+        'description',
+        'completed',
+        'user_id'
+    ];
+
+    /**
+     * Get the user that owns the Task
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
